a1047134c725489ea18c645fb29d26c85d40b2ec
Litpax-Technology <mis@litpaxtechnology.com>
2026-09-12 13:59:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Paste the GAS Web App /exec URL here after deploying Code.gs
-  API_URL: 'https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzieZW4DY_adZ3z01Z8s76QFlVQYpT5BkGU9-WCbO0x2T789i_dU-oxOvxSexrnmgD_MQ/exec',
   APP_NAME: 'Weekly Owner Report',
   COMPANY: 'Litpax Technology'
 };
